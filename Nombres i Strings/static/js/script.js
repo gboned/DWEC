ee44dd4916d59b7cb7e9cc9d7a0d4b78b823c93e
gboned <gboned@cifpfbmoll.eu>
2020-10-09 16:33:08 +0200

    Añadir dos variables globales

---
+++ Nombres i Strings/static/js/script.js
@@ -0,0 +1,5 @@
+// Creo dos variables globales
+// Esta variable obtiene el valor de lo que se introduce dentro del recuadro
+var contenidoTexto = document.getElementsByTagName("input")[0].value;
+// Esta variable apunta al div donde se escribirá el resultado
+var textoResultado = document.getElementById("textResult");
