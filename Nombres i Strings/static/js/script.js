eca046058eefcc7ffdd0a7992acd4f785a0f1194
gboned <gboned@cifpfbmoll.eu>
2020-10-09 16:33:32 +0200

    Añadir función para cuando se da click al botón 1

--- Nombres i Strings/static/js/script.js
+++ Nombres i Strings/static/js/script.js
@@ -3,3 +3,23 @@
 var contenidoTexto = document.getElementsByTagName("input")[0].value;
 // Esta variable apunta al div donde se escribirá el resultado
 var textoResultado = document.getElementById("textResult");
+
+/** 
+ * Al darle click al botón 1, se intentará convertir lo que se
+ * ha introducido en un número, y se indicará si este es igual
+ * a 0, positivo o negativo. En caso de no ser ninguno, se
+ * indicará que hace falta introducir un número
+ */
+function clickButton1() {
+    var contenidoTextoANumero = parseInt(contenidoTexto);
+
+    if (contenidoTextoANumero > 0) {
+        textoResultado.innerText = "El nombre és positiu";
+    } else if (contenidoTextoANumero < 0) {
+        textoResultado.innerText = "El nombre és negatiu";
+    } else if (contenidoTextoANumero === 0) {
+        textoResultado.innerText = "El nombre és igual a 0";
+    } else {
+        textoResultado.innerText = "Has d'escriure un nombre per veure el resultat";
+    }
+}
